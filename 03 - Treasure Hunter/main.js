ff5d160af1954943ab7b1463ad1e74a22bcd0ad3
johannejeils-ek <joje@stud.ek.dk>
2025-10-07 18:48:18 +0200

    Js 03

---
+++ 03 - Treasure Hunter/main.js
@@ -0,0 +1,28 @@
+/*-----------------------------------
+     03 - Treasure Hunter
+ -----------------------------------
+
+Instructions:
+    Island Setup:
+        The island is represented by a 1D array of 5 elements.
+        One of these elements is the hidden treasure (T), and the rest are empty (-).
+*/
+
+const island = ['-', 'T', '-', '-', '-'];
+
+// Log out how many moves there are in the island - level 1
+
+
+
+
+
+/*  For each move log the following out - level 2
+    If there is a treasure log out: Treasure found at move 3 :)
+    If there is not a treasure log out: No treasure found :(
+ */
+
+
+
+
+
+// Log out how many treasures there are in the island - level 3
