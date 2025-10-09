d9f729c59ba8ef00fdf04eefebb53407146a465b
johannejeils-ek <joje@stud.ek.dk>
2025-10-09 09:09:40 +0200

    Treasure Hunter - DONE

--- 03 - Treasure Hunter/main.js
+++ 03 - Treasure Hunter/main.js
@@ -8,21 +8,39 @@ Instructions:
         One of these elements is the hidden treasure (T), and the rest are empty (-).
 */
 
-const island = ['-', 'T', '-', '-', '-'];
-
 // Log out how many moves there are in the island - level 1
-
-
-
-
-
 /*  For each move log the following out - level 2
     If there is a treasure log out: Treasure found at move 3 :)
     If there is not a treasure log out: No treasure found :(
+    // Log out how many treasures there are in the island - level 3
  */
 
+function treasureHunter (island){
+    console.log(`There are ${island.length} moves in the island`)
+    let treasureFound = 0
+
+    island.forEach((move, index)=>{
+        if(move === "T"){
+            console.log(`Treasure found at move ${index + 1} :)`)
+            treasureFound = treasureFound + 1
+        } else {
+            console.log(`No treasure found :(`)
+        }
+        }
+    )
+
+    console.log(`There were ${treasureFound} treasures in the island`)
+}
+
+treasureHunter(['T', '-', 'T', '-', '-'])
 
 
 
 
-// Log out how many treasures there are in the island - level 3
+
+// Callback function = det er en funciton der tager en function som en parameter
+/*
+[].forEach(()=>{})
+
+    [].forEach(function (){} )
+ */
