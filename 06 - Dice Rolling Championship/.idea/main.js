ff3c2f98ec471472d3070d55ba1579c0e27a0bf5
johannejeils-ek <joje@stud.ek.dk>
2025-10-09 10:35:57 +0200

    Dice Rolling Championship - update

---
+++ 06 - Dice Rolling Championship/.idea/main.js
@@ -0,0 +1,13 @@
+/*-----------------------------------
+     06 - Dice Rolling Championship
+ -----------------------------------
+
+Scenario: Two players compete in a dice rolling championship! Each player takes turns rolling a dice, and after 10 rounds, the player with the highest total score wins.
+
+ */
+
+const firstPlayer = prompt("Player 1.")
+const secondPlayer = prompt("Player 2.")
+
+console.log(`Player 1. ${firstPlayer}`)
+console.log(`Player 2. ${secondPlayer}`)
